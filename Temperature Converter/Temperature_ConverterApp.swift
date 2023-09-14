624b5147fdf13b9d3b0bb4251c82697bbea5929b
Ayleen Weiss <ayweit00@hs-esslingen.de>
2023-09-14 15:42:46 +0200

    Initial Commit

---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by Lilly on 14.09.23.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
