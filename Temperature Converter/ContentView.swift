3d32dee6f12198cf24c6137f5fffb0a133ee5635
Ayleen Weiss <ayweit00@hs-esslingen.de>
2023-09-14 16:42:46 +0200

    Add XCFramework and sample UI

--- Temperature Converter/ContentView.swift
+++ Temperature Converter/ContentView.swift
@@ -2,20 +2,59 @@
 //  ContentView.swift
 //  Temperature Converter
 //
-//  Created by Lilly on 14.09.23.
+//  Created by Ayleen on 14.09.23.
 //
 
 import SwiftUI
+import temperatureConverter
 
 struct ContentView: View {
+    var tempConv = TemperatureConverter()
+    @State private var inputTemp: String = ""
+    @State private var resultTemp: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            
+            Text("Convert Celsius to Fahrenheit")
+                .font(.title)
+            
+            TextField("Enter a number", text: $inputTemp)
+                .keyboardType(.numberPad)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+            
+            
+            Button("Convert") {
+                convertTemperature()
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+            TextField("Result", text: $resultTemp)
+                .disabled(true) // So user cannot edit it directly
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+            Spacer()
         }
         .padding()
+    }
+    
+    func convertTemperature() {
+        if let celsius = Double(inputTemp) {
+            let fahrenheit = tempConv.celsiusToFahrenheit(celsius: celsius)
+            resultTemp = String(fahrenheit)
+        } else {
+            resultTemp = "Invalid input"
+        }
     }
 }
 
--- Temperature Converter/Temperature_ConverterApp.swift
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -2,7 +2,7 @@
 //  Temperature_ConverterApp.swift
 //  Temperature Converter
 //
-//  Created by Lilly on 14.09.23.
+//  Created by Ayleen on 14.09.23.
 //
 
 import SwiftUI
